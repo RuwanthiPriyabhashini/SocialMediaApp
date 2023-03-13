22cbfd36e7a94b46e0f208ce715f8bed3db6796f
RuwanthiPriyabhashini <ruwanthipriyabhashini1996@gmail.com>
2023-03-13 05:53:07 +0530

    committed

---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,16 @@
+import "./feed.css"
+import Share from "../Share/Share"
+import Post from "../Post/Post"
+import {Posts} from "../../dummyData"
+
+export default function Feed() {
+  return (
+    <div className ="feed">
+      <div className="feedWrapper"></div>
+      <Share/>
+      {Posts.map((p) =>(
+        <Post key={p.id} post={p} />
+      ))}
+    </div>
+  )
+}
